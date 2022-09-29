0da08cc1c8dcaf6242b8468205cea02eef169d6a
Samuel Dainton <dexterdainton@gmail.com>
2022-09-29 15:43:31 +0000

    Finished app page 1 summary.

--- app_pages/page_1_summary.py
+++ app_pages/page_1_summary.py
@@ -7,33 +7,29 @@ def page_1_summary_body():
     # text based on README file - "Dataset Content" section
     st.info(
         f"**Project Terms & Jargons**\n"
-        f"* A **customer** is a person who consumes your service or product.\n"
-        f"* A **prospect** is a potential customer.\n"
-        f"* A **churned** customer is a user who has stopped using your product or service.\n "
-        f"* This customer, has a **tenure** level, which is the number of months this person " 
-        f"has used our product/service.\n\n"
+        f"* **SalePrice** is the price a house is sold for and is our target variable.\n"
+        f"* There are many abreviated terms used to describe features of the houses in"
+        f"* the data set. In this app we will expand on all of them but for further"
+        f"* clarification the full dataset and explination of it's terms can be found [here](https://www.kaggle.com/datasets/codeinstitute/housing-prices-data).\n\n"
         f"**Project Dataset**\n"
-        f"* The dataset represents a **customer base from a Telco company**. "
-        f"Containing individual customer data on the products and services "
-        f"(like internet type, online security, online backup, tech support), "
-        f"account information (like contract type, payment method, monthly charges) "
-        f"and profile (like gender, partner, dependents).")
+        f"* The dataset has almost 1.5 thousand rows of data and represents housing records from "
+        f"Ames, Iowa; indicating house profile (Floor Area, Basement, Garage, Kitchen, "
+        f"Lot, Porch, Wood Deck, Year Built) and its respective sale price for houses "
+        f"built between 1872 and 2010. "
+        )
 
     # Link to README file, so the users can have access to full project documentation
     st.write(
-        f"* For additional information, please visit and **read** the "
-        f"[Project README file](https://github.com/Code-Institute-Solutions/churnometer).")
+        f"* For additional information, please visit and read the "
+        f"[Project README file](https://github.com/Samuel-Dainton/Heritage-Housing-Issues-P5).")
     
 
     # copied from README file - "Business Requirements" section
     st.success(
         f"The project has 2 business requirements:\n"
-        f"* 1 - The client is interested to understand the patterns from customer base, "
-        f"so that the client can learn the most relevant variables that are correlated to a "
-        f"churned customer.\n"
-        f"* 2 - The client is interested in determining whether or not a given prospect will churn. "
-        f"If so, the client is interested to know when. In addition the client is "
-        f"interested in learning from which cluster this prospect will belong in the customer base. "
-        f"Based on that, present potential factors that could maintain and/or bring  "
-        f"the prospect to a non-churnable cluster."
+        f"* 1 - The client is interested in discovering how house attributes correlate with "
+        f"the typical house Sale Price. Therefore, the client expects data visualizations "
+        f"of the correlated variables against Sale Price to show that.\n"
+        f"* 2 - The client is interested to predict the house sales price from their 4 "
+        f"inherited houses, and any other house in Ames, Iowa. "
         )
