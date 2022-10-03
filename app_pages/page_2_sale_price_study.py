ba36adf425ba7250d5d39f865ffa4f87177852b4
Samuel Dainton <dexterdainton@gmail.com>
2022-10-03 14:43:57 +0000

    Page 2 - Aditional regression plots, tTidy and Finish.

--- app_pages/page_2_sale_price_study.py
+++ app_pages/page_2_sale_price_study.py
@@ -42,52 +42,51 @@ def page_2_sale_price_study_body():
     st.write(
         f"* A correlation study was conducted in the notebook to better understand how "
         f"the variables are correlated to SalePrice. \n"
-        f"Some of the most correlated variable are: **{vars_to_study}**"
+        f"* Further correlation studies were conducted to understand the relation between "
+        f"the quality of a house and the year it was built or remodeled. \n"
+        f"* One final correlation study was conducted to show houses of a similar size "
+        f"across different grades of overall quality against sale price. \n\n"
+        f"Some of the most correlated variables to Sale Price are: **{vars_to_study}**"
     )
 
     # Text based on "02 - Churned Customer Study" notebook - "Conclusions and Next steps" section
     st.info(
-        f"The correlation tests in the notebook and the plots below"
+        f"The correlation tests in the notebook and the plots below "
         f"give the following indications: \n"
-        f"* The SalePrice of a house is typically higher for a house with a larger Ground Living Area \n"
-        f"* The SalePrice of a house is typically higher for a house with a higher Overall Quality  \n"
-        f"* The SalePrice of a house is typically higher for a house with a more recent Year Built. "
-        f"* this is due to houses with a more recent Year Built typically being higher in Overall Quality. \n"
-        f"* Houses with larger sized features such as 1stFlrSF, GarageArea, TotalBsmtSF etc."
-        f"* typically have a larger Ground Living Area. \n"
+        f"* The SalePrice of a house is typically higher for a house with a larger Ground Living Area \n\n"
+        f"* The SalePrice of a house is typically higher for a house with a higher Overall Quality.  "
+        f"This is also true for houses that are of similar size. \n\n"
+        f"* The SalePrice of a house is typically higher for a house with a more recent Year Built or Remodel. "
+        f"This is due to houses with a more recent Year Built or Remodel typically being higher in Overall Quality. \n\n"
     )
-
-
-    df_eda = df.filter(vars_to_study + ['SalePrice'])
 
     # Individual plots per variable
     if st.checkbox("Sale Price correlation per Variable"):
-        churn_level_per_variable(df_eda)
+        df_eda = df.filter(vars_to_study + ['SalePrice'])
+        target_var = 'SalePrice'
+        regression_per_variable(df_eda, target_var)
+
+    if st.checkbox("Overall Quality correlation against Year Built and Remodel"):
+        quality_to_study = ['YearBuilt', 'YearRemodAdd']
+        df_eda = df.filter(quality_to_study + ['OverallQual'])
+        target_var = 'OverallQual'
+        regression_per_variable(df_eda, target_var)
+
+    if st.checkbox("Houses of Similar Area across Quality against Sale Price"):
+        fig, axes = plt.subplots(figsize=(8, 5))
+        fig = sns.lmplot(data=df, x="GrLivArea", y="SalePrice", ci=None, hue='OverallQual')
+        plt.title(f"Houses of Similar Area across Quality", fontsize=20,y=1.05)
+        st.pyplot(fig) 
 
 
-# function created using "02 - Churned Customer Study" notebook code - "Variables Distribution by Churn" section
-def churn_level_per_variable(df_eda):
-    target_var = 'SalePrice'
+def regression_per_variable(df_eda, target_var):
     
     for col in df_eda.drop([target_var], axis=1).columns.to_list():
-        if df_eda[col].dtype == 'object':
-            plot_categorical(df_eda, col, target_var)
-        else:
             plot_numerical(df_eda, col, target_var)
 
 
-# code copied from "02 - Churned Customer Study" notebook - "Variables Distribution by Churn" section
-def plot_categorical(df, col, target_var):
-    fig, axes = plt.subplots(figsize=(12, 5))
-    sns.countplot(data=df, x=col, hue=target_var,order = df[col].value_counts().index)
-    plt.xticks(rotation=90) 
-    plt.title(f"{col}", fontsize=20,y=1.05)        
-    st.pyplot(fig) # st.pyplot() renders image, in notebook is plt.show()
-
-
-# code copied from "02 - Churned Customer Study" notebook - "Variables Distribution by Churn" section
 def plot_numerical(df, col, target_var):
     fig, axes = plt.subplots(figsize=(8, 5))
     fig = sns.lmplot(data=df, x=col, y=target_var, ci=None) 
     plt.title(f"{col}", fontsize=20,y=1.05)
-    st.pyplot(fig) # st.pyplot() renders image, in notebook is plt.show()
+    st.pyplot(fig) 
