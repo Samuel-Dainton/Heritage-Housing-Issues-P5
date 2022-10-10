e12b90cf1b77796277f75f212faf85675964ee15
Samuel Dainton <dexterdainton@gmail.com>
2022-10-10 17:02:00 +0000

    Streamlit Page 5 & evaluate_pipeline. Added code to flatten Y train and test sets to 1D array to fix value error.

--- src/machine_learning/evaluate_pipeline.py
+++ src/machine_learning/evaluate_pipeline.py
@@ -14,26 +14,30 @@ def regression_performance(X_train, y_train, X_test, y_test,pipeline):
 	st.write(regression_evaluation(X_test,y_test,pipeline))
 
 def regression_evaluation(X,y,pipeline):
-  prediction = pipeline.predict(X)
-  st.write('R2 Score:', r2_score(y, prediction).round(3))  
-  st.write('Mean Absolute Error:', mean_absolute_error(y, prediction).round(3))
+ 	prediction = pipeline.predict(X)
+ 	st.write('R2 Score:', r2_score(y, prediction).round(3))  
+ 	st.write('Mean Absolute Error:', mean_absolute_error(y, prediction).round(3))
 
 def regression_evaluation_plots(X_train, y_train, X_test, y_test,pipeline, alpha_scatter=0.5):
-  pred_train = pipeline.predict(X_train)
-  pred_test = pipeline.predict(X_test)
 
+	y_train = y_train.to_numpy().flatten()
+	y_test = y_test.to_numpy().flatten()
+	
+	pred_train = pipeline.predict(X_train)
+	pred_test = pipeline.predict(X_test)
+	
 
-  fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12,6))
-  sns.scatterplot(x=y_train , y=pred_train, alpha=alpha_scatter, ax=axes[0])
-  sns.lineplot(x=y_train , y=y_train, color='red', ax=axes[0])
-  axes[0].set_xlabel("Actual")
-  axes[0].set_ylabel("Predictions")
-  axes[0].set_title("Train Set")
+	fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12,6))
+	sns.scatterplot(x=y_train , y=pred_train, alpha=alpha_scatter, ax=axes[0])
+	sns.lineplot(x=y_train , y=y_train, color='red', ax=axes[0])
+	axes[0].set_xlabel("Actual")
+	axes[0].set_ylabel("Predictions")
+	axes[0].set_title("Train Set")
 
-  sns.scatterplot(x=y_test , y=pred_test, alpha=alpha_scatter, ax=axes[1])
-  sns.lineplot(x=y_test , y=y_test, color='red', ax=axes[1])
-  axes[1].set_xlabel("Actual")
-  axes[1].set_ylabel("Predictions")
-  axes[1].set_title("Test Set")
+	sns.scatterplot(x=y_test , y=pred_test, alpha=alpha_scatter, ax=axes[1])
+	sns.lineplot(x=y_test , y=y_test, color='red', ax=axes[1])
+	axes[1].set_xlabel("Actual")
+	axes[1].set_ylabel("Predictions")
+	axes[1].set_title("Test Set")
 
-  st.pyplot(fig)
+	st.pyplot(fig)
